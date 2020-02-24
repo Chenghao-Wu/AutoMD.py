cf0db7a215487a38fd1f93caf3bfdb448789404e
Zhenghao <w415146142@gmail.com>
2020-02-24 11:33:13 +0100

    rm test

--- test/input_scripts/galamost/test_argparser.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import argparse
-
-parser = argparse.ArgumentParser(description='galamost.')
-
-parser.add_argument('-directory', type=str,nargs=1,
-                   help='directory for the simulation')
